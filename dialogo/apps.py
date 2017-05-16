d843944a489eb717989571d99f50884a888fc6b4
Jonhnatha Trigueiro <joepreludian@gmail.com>
2017-05-16 10:34:08 -0300

    Commit inicial

---
+++ dialogo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DialogoConfig(AppConfig):
+    name = 'dialogo'
---
+++ parlamentar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ParlamentarConfig(AppConfig):
+    name = 'parlamentar'
